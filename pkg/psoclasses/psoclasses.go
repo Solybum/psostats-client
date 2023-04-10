e41ddd37f64d2e1c43473a0f30b9f9931f53ed7e
wilson <44687779+cmwilson252@users.noreply.github.com>
2023-04-10 15:00:42 -0500

    ultima class stats

--- pkg/psoclasses/psoclasses.go
+++ pkg/psoclasses/psoclasses.go
@@ -10,18 +10,18 @@ type PsoClass struct {
 	Ata       int
 }
 
-var HUmar = PsoClass{Name: "HUmar", MaxShifta: 3, MinAtp: 1392, MaxAtp: 1397, Ata: 200}
-var HUnewearl = PsoClass{Name: "HUnewearl", MaxShifta: 20, MinAtp: 1232, MaxAtp: 1237, Ata: 199}
-var HUcast = PsoClass{Name: "HUcast", MaxShifta: 3, MinAtp: 1634, MaxAtp: 1639, Ata: 191}
-var HUcaseal = PsoClass{Name: "HUcaseal", MaxShifta: 3, MinAtp: 1296, MaxAtp: 1301, Ata: 218}
-var RAmar = PsoClass{Name: "RAmar", MaxShifta: 15, MinAtp: 1256, MaxAtp: 1260, Ata: 249}
-var RAmarl = PsoClass{Name: "RAmarl", MaxShifta: 20, MinAtp: 1141, MaxAtp: 1145, Ata: 241}
-var RAcast = PsoClass{Name: "RAcast", MaxShifta: 0, MinAtp: 1346, MaxAtp: 1350, Ata: 224}
-var RAcaseal = PsoClass{Name: "RAcaseal", MaxShifta: 0, MinAtp: 1171, MaxAtp: 1175, Ata: 231}
-var FOmar = PsoClass{Name: "FOmar", MaxShifta: 30, MinAtp: 1000, MaxAtp: 1002, Ata: 163}
-var FOmarl = PsoClass{Name: "FOmarl", MaxShifta: 30, MinAtp: 870, MaxAtp: 872, Ata: 170}
-var FOnewm = PsoClass{Name: "FOnewm", MaxShifta: 30, MinAtp: 812, MaxAtp: 814, Ata: 180}
-var FOnewearl = PsoClass{Name: "FOnewearl", MaxShifta: 30, MinAtp: 581, MaxAtp: 583, Ata: 186}
+var HUmar = PsoClass{Name: "HUmar", MaxShifta: 7, MinAtp: 1492, MaxAtp: 1497, Ata: 210}
+var HUnewearl = PsoClass{Name: "HUnewearl", MaxShifta: 20, MinAtp: 1332, MaxAtp: 1337, Ata: 209}
+var HUcast = PsoClass{Name: "HUcast", MaxShifta: 3, MinAtp: 1734, MaxAtp: 1739, Ata: 201}
+var HUcaseal = PsoClass{Name: "HUcaseal", MaxShifta: 7, MinAtp: 1396, MaxAtp: 1401, Ata: 228}
+var RAmar = PsoClass{Name: "RAmar", MaxShifta: 15, MinAtp: 1356, MaxAtp: 1360, Ata: 259}
+var RAmarl = PsoClass{Name: "RAmarl", MaxShifta: 20, MinAtp: 1241, MaxAtp: 1245, Ata: 251}
+var RAcast = PsoClass{Name: "RAcast", MaxShifta: 7, MinAtp: 1446, MaxAtp: 1450, Ata: 234}
+var RAcaseal = PsoClass{Name: "RAcaseal", MaxShifta: 7, MinAtp: 1271, MaxAtp: 1275, Ata: 241}
+var FOmar = PsoClass{Name: "FOmar", MaxShifta: 30, MinAtp: 1100, MaxAtp: 1102, Ata: 173}
+var FOmarl = PsoClass{Name: "FOmarl", MaxShifta: 30, MinAtp: 970, MaxAtp: 972, Ata: 180}
+var FOnewm = PsoClass{Name: "FOnewm", MaxShifta: 30, MinAtp: 912, MaxAtp: 914, Ata: 190}
+var FOnewearl = PsoClass{Name: "FOnewearl", MaxShifta: 30, MinAtp: 681, MaxAtp: 683, Ata: 196}
 
 func ForName(name string) (PsoClass, error) {
 	switch name {
